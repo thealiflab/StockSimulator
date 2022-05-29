5c3cb0e8a2c4a2ae32f157f1bd7569b09f520436
alif <ahmedalif1010@gmail.com>
2022-05-30 00:47:08 +0600

    graph prepared as it should be

--- graph.py
+++ graph.py
@@ -1,23 +1,33 @@
 import tkinter
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
+from matplotlib.ticker import MaxNLocator
 import random
 
 window = tkinter.Tk()
 window.title("Matplotlib in Tkinter")
 window.minsize(640, 400)
 
-n = random.randint(0, 100)
-x_list = [1]
-y_list = [3]
 
+x_list = [1]
+y_list = [1]
+
+def axis_draw():
+    ax.clear()
+
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+    ax.plot(x_list, y_list)
+
+    ax.set_xticks(range(0, 11))
+    ax.set_yticks(range(0, 11))
 
 def graph_extend():
-    x_list.append(random.randint(1, 10))
+    x_list.append(x_list[-1]+1)
     y_list.append(random.randint(1, 10))
 
-    ax.clear()
-    ax.plot(x_list, y_list)  # inform matplotlib of the new data
+    axis_draw()
+
     canvas.draw()  # redraw
 
     print(x_list)
@@ -26,7 +36,9 @@ def graph_extend():
 
 fig = Figure(figsize=(5, 5), dpi=100)
 ax = fig.add_subplot(1, 1, 1)
-ax.plot(x_list, y_list)
+ax.set(xlabel='Day', ylabel='Price')
+
+axis_draw()
 
 canvas = FigureCanvasTkAgg(fig, master=window)
 canvas.draw()
